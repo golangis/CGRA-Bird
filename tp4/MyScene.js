4736798737a6cfb8d1b8aa418cd476b9eb7930c6
Mariana Rocha <up202004656@edu.fe.up.pt>
2023-03-20 10:08:27 +0000

    cube exercise

--- tp4/MyScene.js
+++ tp4/MyScene.js
@@ -1,7 +1,7 @@
 import { CGFscene, CGFcamera, CGFaxis, CGFappearance, CGFtexture } from "../lib/CGF.js";
 import { MyQuad } from "./MyQuad.js";
 import { MyTangram } from "./MyTangram.js";
-
+import { MyUnitCubeQuad } from "./MyUnitCubeQuad.js";
 /**
  * MyScene
  * @constructor
@@ -29,6 +29,7 @@ export class MyScene extends CGFscene {
         this.axis = new CGFaxis(this);
         this.quad = new MyQuad(this);
         this.tangram = new MyTangram(this);
+        this.unitCubeQuad = new MyUnitCubeQuad(this);
 
         //------ Applied Material
         this.quadMaterial = new CGFappearance(this);
@@ -128,7 +129,8 @@ export class MyScene extends CGFscene {
 
         // this.quad.display();
 
-        this.tangram.display();
+        // this.tangram.display();
+        this.unitCubeQuad.display();
 
         // ---- END Primitive drawing section
     }
